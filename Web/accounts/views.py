39618afa909617112e3c3724934c4d4288a8b32d
PLH0319 <147641986+PLH0319@users.noreply.github.com>
2024-07-18 16:55:27 +0800

    Update login_next
    Update login to next page

--- Web/accounts/views.py
+++ Web/accounts/views.py
@@ -3,8 +3,16 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 
+
 def user_login(request):
-    print(request.method)
+    def get_success_url(request):
+        next_url = request.POST.get('next')
+        if next_url:
+            print(next_url)
+            return redirect(next_url)
+        else:
+            return redirect('/')  # default URL to redirect to after login
+
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -13,7 +21,7 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return get_success_url(request)
     else:
         form = AuthenticationForm()
         print('no login')
@@ -25,9 +33,7 @@ def user_logout(request):
 
 def register(request):
     if request.method == 'POST':
-        print(request.POST)
         form = UserCreationForm(data=request.POST)
-        print(form)
         if form.is_valid():
             new_user = form.save()
             # authenticate_user = authenticate(username=new_user.username,
